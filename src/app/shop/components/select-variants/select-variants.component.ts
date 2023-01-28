9cb2deec56308e29802aa52374c1cf43c3a5c489
JeremyVk <66004588+JeremyVk@users.noreply.github.com>
2023-01-28 17:44:55 +0100

    product page (#19)

---
+++ src/app/shop/components/select-variants/select-variants.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BaseVariant } from '../../interfaces/baseVariant';
+
+@Component({
+  selector: 'app-select-variants',
+  templateUrl: './select-variants.component.html',
+  styleUrls: ['./select-variants.component.css']
+})
+export class SelectVariantsComponent implements OnInit {
+  @Input() variants: Array<BaseVariant> = [];
+  @Output() eventSelectedVariant = new EventEmitter<BaseVariant>();
+
+  displayVariants:boolean = false;
+  selectedVariant: BaseVariant = {};
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.variants);
+    this.selectedVariant = this.variants[0];
+  }
+
+  toggleDisplayVariants() {
+    this.displayVariants = !this.displayVariants    
+  }
+
+  changeSelectedVariant(variant: BaseVariant) {
+    this.selectedVariant = variant;
+    this.eventSelectedVariant.emit(variant);
+  }
+
+}
