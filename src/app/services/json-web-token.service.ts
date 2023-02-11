6e6a60e8f8abd6a45548bd070ac6cbe10147e90b
JeremyVk <66004588+JeremyVk@users.noreply.github.com>
2023-02-11 13:55:06 +0100

    refresh jwt (#27)

--- src/app/services/json-web-token.service.ts
+++ src/app/services/json-web-token.service.ts
@@ -1,26 +1,64 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+import { environment } from 'src/environments/environment';
+import { Jwt } from '../shop/interfaces/jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonWebTokenService {
-
-  constructor() { }
+  refreshTokenUrl: string = `${environment.url}/api/token/refresh`
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   getJsonWebToken(): string | null {
     return localStorage.getItem('JWT');
+  }
+
+  getRefreshToken(): string | null {
+    return localStorage.getItem('refresh_token');
   }
 
   setJsonWebToken(jwt: string):void {
     localStorage.setItem('JWT', jwt);
   }
 
+  setRefreshWebToken(refreshToken: string):void {    
+    localStorage.setItem('refresh_token', refreshToken);
+}
+
   deleteJsonWebToken():void {
     localStorage.removeItem('JWT');
     localStorage.removeItem('user');
+    localStorage.removeItem('refresh_token');
   }
 
   hasJsonWebToken(): boolean {
     return localStorage.getItem('JWT') !== null;
   }
+
+  isJsonWebTokenExpired(token: string): boolean {
+      let decryptedToken: Jwt = jwt_decode(token);
+      const now = new Date().getTime() / 1000;
+
+      return now > decryptedToken.exp;
+  }
+
+  isRefreshTokenExpired(refreshToken: string): boolean {
+    const token = this.getJsonWebToken();
+    if(token) {
+      let decryptedToken: Jwt = jwt_decode(token);
+      const now = new Date().getTime() / 1000;
+      return now > decryptedToken.exp;
+    }
+    return true;
+  }
+
+  refreshJwtToken(refresh_token: string) {
+    return this.http.post<{token: string, refresh_token: string}>(`${environment.url}/api/token/refresh`, {"refresh_token": refresh_token})
+  }
+
+
 }
