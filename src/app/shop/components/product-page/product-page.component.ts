3e099aa8fba6950a03c877636e0fe17278280530
JeremyVk <jeremvk@outlook.fr>
2023-03-20 16:01:12 +0100

    product page notification add to cart, prod environment

--- src/app/shop/components/product-page/product-page.component.ts
+++ src/app/shop/components/product-page/product-page.component.ts
@@ -5,7 +5,9 @@ import { Article } from '../../interfaces/article';
 import { BaseVariant } from '../../interfaces/baseVariant';
 import { Book } from '../../interfaces/book';
 import { CartService } from '../../services/cart/cart.service';
+import { NotificationsService } from '../../services/notifications/notifications.service';
 import { ProductService } from '../../services/product/product.service';
+import { Notification } from 'src/app/shop/interfaces/notification';
 
 @Component({
   selector: 'app-product-page',
@@ -22,7 +24,8 @@ export class ProductPageComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-    private cartService: CartService
+    private cartService: CartService,
+    private notificationService: NotificationsService
     ) { }
 
   ngOnInit(): void {
@@ -45,5 +48,8 @@ export class ProductPageComponent implements OnInit {
 
   addToCart() {
     this.cartService.addProductToCart(this.selectedVariant);
+    let notification: Notification = {}
+    notification.text = "Votre article a bien été ajouté au panier"
+    this.notificationService.pushNotification(notification)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://127.0.0.1:8000',
+  baseShippingAmount: 400,
+  productImagesUrl: 'http://127.0.0.1:8000/images/products/'
 };
