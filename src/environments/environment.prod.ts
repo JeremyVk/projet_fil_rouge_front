04689643d89b2dbd9b69339af90ee1094383a337
JeremyVk <66004588+JeremyVk@users.noreply.github.com>
2023-08-31 22:20:09 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: 'https://www.l2i.backend.jeremy-vk.fr',
   baseShippingAmount: 400,
-  productImagesUrl: 'https://www.l2i.backend.jeremy-vk.fr/images/products'
+  productImagesUrl: 'https://www.l2i.backend.jeremy-vk.fr/images/products/'
 };
